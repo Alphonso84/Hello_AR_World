a1f37d2674bc41ebe427622b84b47d187d1e1485
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2021-01-03 16:46:22 -0800

    First Commit- -Added map of earth image overlay onto sphere node -Added rotation action to allow earth sphere to rotate indefinitely.

--- Hello_AR_World/ViewController.swift
+++ Hello_AR_World/ViewController.swift
@@ -22,11 +22,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        //sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        sceneView.autoenablesDefaultLighting = true
+       
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,6 +35,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration)
+        drawSphereAtOrigin()
+//        drawBoxAt12oclockHigh()
+//        drawPyramid()
+//        drawPlane()
+//        drawTorusAt300()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -44,6 +47,54 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func drawPlane() {
+        let plane = SCNNode(geometry: SCNPlane(width: 1, height: 1))
+        plane.position = SCNVector3(-0.3, 0, -1)
+        plane.eulerAngles = SCNVector3(-45.degreesToRadians(),20.degreesToRadians(),-45.degreesToRadians())
+        plane.geometry?.firstMaterial?.diffuse.contents = UIImage(named:"ChelseaAndFonzWedding")
+        plane.geometry?.firstMaterial?.specular.contents = UIColor.white
+        sceneView.scene.rootNode.addChildNode(plane)
+    }
+    
+    func drawSphereAtOrigin() {
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.05))
+        let sphere2 = SCNNode(geometry: SCNSphere(radius: 0.10))
+        sphere.geometry?.firstMaterial?.diffuse.contents = UIImage(named:"earthmap")
+        sphere.position = SCNVector3(0,0,0)
+        sphere.eulerAngles = SCNVector3(0,23.degreesToRadians(),0)
+        sceneView.scene.rootNode.addChildNode(sphere)
+        let rotateAction = SCNAction.rotate(by: 720.degreesToRadians(), around: SCNVector3(0, 1, 0), duration: 60)
+        let rotateForeverAction = SCNAction.repeatForever(rotateAction)
+        sphere.runAction(rotateForeverAction)
+        //sceneView.scene.rootNode.addChildNode(sphere2)
+    }
+    
+    func drawBoxAt12oclockHigh() {
+        let box = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 00))
+        box.position = SCNVector3(0, 0.2, -0.3)
+        box.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
+        box.geometry?.firstMaterial?.specular.contents = UIColor.white
+        sceneView.scene.rootNode.addChildNode(box)
+
+    }
+    
+    func drawPyramid() {
+        let pyramid = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
+        pyramid.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+        pyramid.geometry?.firstMaterial?.specular.contents = UIColor.red
+        pyramid.position = SCNVector3(0, -0.2, 0.3)
+        sceneView.scene.rootNode.addChildNode(pyramid)
+    }
+    
+    func drawTorusAt300() {
+        let torus = SCNNode(geometry: SCNTorus(ringRadius: 0.05, pipeRadius: 0.03))
+        torus.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        torus.geometry?.firstMaterial?.specular.contents = UIColor.white
+        torus.position = SCNVector3(0.2,0,0)
+        torus.eulerAngles = SCNVector3(0, 0, 45.degreesToRadians())
+        sceneView.scene.rootNode.addChildNode(torus)
     }
 
     // MARK: - ARSCNViewDelegate
@@ -70,5 +121,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+}
+
+extension Int {
+    func degreesToRadians() -> CGFloat {
+        return CGFloat(self) * CGFloat.pi / 180.0
     }
 }
